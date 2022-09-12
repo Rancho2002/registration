c20b40fc50e8519028eb939c2f2dd440213d890b
Arijit <subhasghosh196@outlook.com>
2022-09-13 02:09:57 +0530

    modified a lot and validation

--- registrationForm/index.php
+++ registrationForm/index.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Asia/Kolkata');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   include "../parts/_dbconnect.php";
   $gender = $_POST['gender'];
-  $roll =  $_POST['roll'];
+  // $roll =  $_POST['roll'];
   $name =  $_POST['name'];
   $gender =  $_POST['gender'];
   $dept =  $_POST['dept'];
@@ -12,6 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $mname =  $_POST['mname'];
   $acat =  $_POST['acat'];
   $ocat = $_POST['ocat'];
+  $domicile = $_POST['domicile'];
   $dob = $_POST['dob'];
   $alotrank = $_POST['alotrank'];
   $gmr = $_POST['gmr'];
@@ -25,6 +26,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $fcbi = $_POST['fcbi'];
 
   $date=date('Y-m-d H:i:s');
+
+  $total=mysqli_query($conn,"SELECT * FROM `registration`");
+  $roll=mysqli_num_rows($total);
+  $roll++;
 
   $newRoll = '350/' . date("Y") . '/' . $dept . '/' . $roll;
   $sql = "INSERT INTO `registration` (`roll`, `name`,`gender`, `dept`, `fname`, `mname`, `acat`, `ocat`, `dob`, `alotrank`, `gmr`, `doa`, `mob`, `gmob`, `address`, `aadhar`, `mail`, `fsbi`, `fcbi`) VALUES ( '$newRoll', '$name', '$gender','$dept', '$fname', '$mname', '$acat', '$ocat', '$dob', '$alotrank', '$gmr', '$date', '$mob', '$gmob', '$address', '$aadhar', '$mail', '$fsbi', '$fcbi')";
@@ -55,133 +60,207 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 
 <body class="bg-secondary">
-  <?php
+<?php
+// echo "pwd-"."gen";
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
 
   echo '
-  <form id="regForm" action="/registration/registrationForm/index.php/" method="POST" class="bg-dark text-white container my-3">
+  <form id="regForm" action="/registration/registrationForm/index.php/" method="POST" class="bg-dark text-white container my-3 needs-validation" novalidate>
 
-    <h1 class="text-center">Student Registration</h1>
+  <h1 class="text-center">Student Admission</h1>
 
-    <!-- One "tab" for each step in the form: -->
-    <div class="form-group">
-      <div class="tab">
+  <!-- One "tab" for each step in the form: -->
+  <div class="form-group">
+    <div class="tab">
 
-        <label for="name">Name</label>
-        <input class="form-control my-1" placeholder="Name" name="name">
-        <label for="Provisional Roll No" class="my-1">Provisional Roll No</label>
-        <input class="form-control my-1" type="number" name="roll" placeholder="Provisional Roll No">
-
-
-
-        <label for="department" class="my-1">Gender</label>
-        <select class="custom-select form-control my-1" id="gender" name="gender" required>
-          <option selected disabled>Choose...</option>
-          <option value="M">M</option>
-          <option value="F">F</option>
-        </select>
-
-        <label for="department" class="my-1">Department</label>
-        <select class="custom-select form-control my-1" id="dept" name="dept" required>
-          <option selected disabled>Choose...</option>
-          <option value="CSE">CSE</option>
-          <option value="ECE">ECE</option>
-          <option value="ME">ME</option>
-          <option value="EE">EE</option>
-          <option value="CE">CE</option>
-        </select>
-
-        <label for="father name" class="my-1">Father\'s Name</label>
-        <input class="form-control my-1" placeholder="Father\'s Name" name="fname">
-
-        <label for="mother name" class="my-1">Mother\'s Name</label>
-        <input class="form-control my-1" placeholder="Mother\'s Name" name="mname">
-
-        <label for="date of birth" class="my-1">Date of Birth</label>
-        <input class="form-control my-1" type="date" placeholder="Date of birth" name="dob">
+      <label for="name">Name</label>
+      <input class="form-control my-1" placeholder="Name" name="name" minlength="6" required>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Please provide a valid name
+      </div>
+      <!--<label for="Provisional Roll No" class="my-1">Provisional Roll No</label>
+      <input class="form-control my-1" type="number" name="roll" placeholder="Provisional Roll No">-->
 
 
 
+      <label for="department" class="my-1">Gender</label>
+      <select class="custom-select form-control my-1" id="gender" name="gender" required>
+      <option selected disabled value="">Choose...</option>
+        <option value="M">M</option>
+        <option value="F">F</option>
+      </select>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Please provide a valid gender
       </div>
 
-      <div class="tab">
-        <label for="alloted category" class="my-1">Alloted Category</label>
-        <select class="custom-select form-control my-1" id="acat" name="acat">
-          <option selected disabled>Choose...</option>
-          <option value="GEN">GEN</option>
-          <option value="SC">SC</option>
-          <option value="ST">ST</option>
-          <option value="OBC-A">OBC-A</option>
-          <option value="OBC-B">OBC-B</option>
-          <option value="TFW">TFW</option>
-          <option value="ARMY">ARMY</option>
-        </select>
-
-        <label for="original category" class="my-1">Original Category</label>
-        <select class="custom-select form-control my-1" id="ocat" name="ocat">
-          <option selected disabled>Choose...</option>
-          <option value="GEN">GEN</option>
-          <option value="SC">SC</option>
-          <option value="ST">ST</option>
-          <option value="OBC-A">OBC-A</option>
-          <option value="OBC-B">OBC-B</option>
-        </select>
-        <label for="alloted rank" class="my-1">Alloted Rank</label>
-        <input class="form-control my-1" type="number" placeholder="Alloted Rank" name="alotrank">
-        <label for="gmr" class="my-1">General Merit Rank</label>
-        <input class="form-control my-1" type="number" placeholder="General Merit Rank" name="gmr">
-        <!-- <label for="date of admission" class="my-1">Date of Admission</label> -->
+      <label for="department" class="my-1">Department</label>
+      <select class="custom-select form-control my-1" id="dept" name="dept" required>
+      <option selected disabled value="">Choose...</option>
+        <option value="CSE">CSE</option>
+        <option value="ECE">ECE</option>
+        <option value="ME">ME</option>
+        <option value="EE">EE</option>
+        <option value="CE">CE</option>
+      </select>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Please provide a valid department
       </div>
 
-      <div class="tab">
-        <label for="contact info" class="my-2">Contact Info:</label>
-        <input class="form-control my-3" type="email" placeholder="Email" name="mail">
-        <input class="form-control my-3" placeholder="phone" type="number" name="mob">
-        <input class="form-control my-3" placeholder="Guardian Phone" type="number" name="gmob">
-        <input class="form-control my-3" placeholder="Address" name="address">
-        <input class="form-control my-3" placeholder="Aadhar" type="number" minlength="12" name="aadhar">
-        <label for="exampleInputEmail1" class="my-2">Admission Fees:</label>
-        <input class="form-control my-3" type="number" placeholder="SBI" name="fsbi">
-        <input class="form-control my-3" type="number" placeholder="CBI" name="fcbi">
+      <label for="father name" class="my-1">Father\'s Name</label>
+      <input class="form-control my-1" placeholder="Father\'s Name" minlength="6" name="fname" required>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Please provide a valid name
+      </div>
+
+      <label for="mother name" class="my-1">Mother\'s Name</label>
+      <input class="form-control my-1" placeholder="Mother\'s Name" minlength="6" name="mname" required>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Please provide a valid name
+      </div>
+
+      <label for="date of birth" class="my-1">Date of Birth</label>
+      <input class="form-control my-1" type="date" placeholder="Date of birth" name="dob" required>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Please provide a valid date of birth
       </div>
 
 
+    </div>
 
-      <!-- <div class="tab">
-        <label for="exampleInputEmail1" class="my-2">Admission Fees:</label>
-        <input class="form-control my-3" type="number" placeholder="SBI">
-        <input class="form-control my-3" type="number" placeholder="CBI">
-        <label for="exampleInputEmail1" class="my-2">Semester Fees:</label>
-        <input class="form-control my-3" type="number" placeholder="1st sem">
-        <input class="form-control my-3" type="number" placeholder="2nd sem">
-        <input class="form-control my-3" type="number" placeholder="3rd sem">
-        <input class="form-control my-3" type="number" placeholder="4th sem">
-        <input class="form-control my-3" type="number" placeholder="5th sem">
+   
+
+      <label for="alloted category" class="my-1">Alloted Category</label>
+      <select class="custom-select form-control my-1" id="acat" name="acat" required>
+      <!-- <option selected disabled value="">Choose...</option> -->
+        <option value="GEN" selected>GEN</option>
+        <option value="SC">SC</option>
+        <option value="ST">ST</option>
+        <option value="OBC-A">OBC-A</option>
+        <option value="OBC-B">OBC-B</option>
+        <option value="TFW">TFW</option>
+        <option value="DEFENCE">DEFENCE</option>
+      </select>
+      <div class="valid-feedback">
+        Looks good!
       </div>
-      <div class="tab">
-        <input class="form-control my-3" type="number" placeholder="6th sem">
-        <input class="form-control my-3" type="number" placeholder="7th sem">
-        <input class="form-control my-3" type="number" placeholder="8th sem">
-      </div> -->
-      <div style="overflow:auto;">
-        <div style="float:right;">
-          <!--<button type="button" id="prevBtn" onclick="nextPrev(-1)" class="btn btn-primary mt-2 mr-2 px-3">Previous</button>
-          <button type="button" id="nextBtn" onclick="nextPrev(1)" class="btn btn-primary mt-2 px-3">Next</button>-->
-          <button type="Submit" class="btn btn-primary my-2 px-3">Submit</button>
-        </div>
+      <div class="invalid-feedback">
+        Please provide a valid category
       </div>
 
-      <!-- Circles which indicates the steps of the form: -->
-      <div style="text-align:center;margin-top:40px;">
-        <span class="step"></span>
-        <span class="step"></span>
-        <span class="step"></span>
-        <!-- <span class="step"></span> -->
+      <label for="pwd" class="my-1 mt-2">PWD</label>
+      <div class="custom-control custom-radio">
+    <input type="radio" class="custom-control-input" id="customControlValidation2" name="radio-stacked" value="yes" required>
+    <label class="custom-control-label" for="customControlValidation2">Yes</label>
+  </div>
+  <div class="custom-control custom-radio mb-3">
+    <input type="radio" class="custom-control-input" id="customControlValidation3" name="radio-stacked" value="no" required>
+    <label class="custom-control-label" for="customControlValidation3">No</label>
+    <div class="invalid-feedback">This field must be selected</div>
+  </div>
+
+
+
+      <label for="original category" class="my-1 needs-validation">Original Category</label>
+      <select class="custom-select form-control my-1" id="ocat" name="ocat" required>
+      <!--<option selected disabled value="">Choose...</option>-->
+        <option value="GEN" selected>GEN</option>
+        <option value="SC">SC</option>
+        <option value="ST">ST</option>
+        <option value="OBC-A">OBC-A</option>
+        <option value="OBC-B">OBC-B</option>
+      </select>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Please provide a valid category
       </div>
 
-  </form>
-  </div>';
-}
+      <label for="original category" class="my-1 needs-validation">Domicile</label>
+      <select class="custom-select form-control my-1" id="domicile" name="domicile" required>
+        <!-- <option selected disabled value="">Choose...</option> -->
+        <option value="West Bengal">West Bengal</option>
+        <option value="Outside">Outside</option>
+      </select>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+      <div class="invalid-feedback">
+        Please provide a valid status
+      </div>
+
+      <label for="alloted rank" class="my-1">Alloted Rank</label>
+      <input class="form-control my-1" type="number" placeholder="Alloted Rank" name="alotrank" required>
+   
+      <label for="gmr" class="my-1">General Merit Rank</label>
+      <input class="form-control my-1" type="number" placeholder="General Merit Rank" name="gmr" required>
+     
+      <!-- <label for="date of admission" class="my-1">Date of Admission</label> -->
+    </div>
+
+    <div class="tab">
+      <label for="contact info" class="my-2">Contact Info:</label>
+      <input class="form-control my-3" type="email" placeholder="Email" name="mail" required>
+      <input class="form-control my-3" placeholder="phone" type="number" name="mob" required>
+      <input class="form-control my-3" placeholder="Guardian Phone" type="number" name="gmob" required>
+      <input class="form-control my-3" placeholder="Address" name="address" required>
+      <input class="form-control my-3" placeholder="Aadhar" type="number" minlength="12" name="aadhar" required>
+      <label for="exampleInputEmail1" class="my-2">Admission Fees:</label>
+      <input class="form-control my-3" type="number" placeholder="SBI" name="fsbi" required>
+      <input class="form-control my-3" type="number" placeholder="CBI" name="fcbi" required>
+    </div>
+
+
+
+    <!-- <div class="tab">
+      <label for="exampleInputEmail1" class="my-2">Admission Fees:</label>
+      <input class="form-control my-3" type="number" placeholder="SBI">
+      <input class="form-control my-3" type="number" placeholder="CBI">
+      <label for="exampleInputEmail1" class="my-2">Semester Fees:</label>
+      <input class="form-control my-3" type="number" placeholder="1st sem">
+      <input class="form-control my-3" type="number" placeholder="2nd sem">
+      <input class="form-control my-3" type="number" placeholder="3rd sem">
+      <input class="form-control my-3" type="number" placeholder="4th sem">
+      <input class="form-control my-3" type="number" placeholder="5th sem">
+    </div>
+    <div class="tab">
+      <input class="form-control my-3" type="number" placeholder="6th sem">
+      <input class="form-control my-3" type="number" placeholder="7th sem">
+      <input class="form-control my-3" type="number" placeholder="8th sem">
+    </div> -->
+    <div style="overflow:auto;">
+      <div style="float:right;">
+        <!--<button type="button" id="prevBtn" onclick="nextPrev(-1)" class="btn btn-primary mt-2 mr-2 px-3">Previous</button>
+        <button type="button" id="nextBtn" onclick="nextPrev(1)" class="btn btn-primary mt-2 px-3">Next</button>-->
+        <button  type="submit" value="Submit" class="btn btn-primary my-2 px-3">Submit</button>
+      </div>
+    </div>
+
+    
+
+</form>
+</div>
+
+';
+} 
 else{
   header("location: /registration/teacher.php/?status=notlogin");
 }
@@ -192,6 +271,26 @@ else{
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
   <!-- <script src="script.js"></script> -->
+  <script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
 </body>
 
 </html>
